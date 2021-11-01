5bf22c1a6285ab935bc519b8b678888d6d101df4
baderbuddy <baderbuddy@gmail.com>
2021-11-01 20:22:46 +0000

    Fixed code

--- main.py
+++ main.py
@@ -7,9 +7,14 @@ from flask import (
     session,
     url_for,
     Response,
+    Flask,
 )
 from twilio.twiml.voice_response import VoiceResponse
 import random
+
+app = Flask(__name__)
+
+
 class Maze:
     def __init__(self):
         self.north = None
@@ -17,12 +22,18 @@ class Maze:
         self.west = None
         self.south = None
         self.final = False
+
     def __str__(self):
         return str(self.index) + " " + str(self.final)
+
     def __repr__(self):
         return self.__str__()
+
+
 maze = []
 mazeSize = 80
+
+
 def generateMaze():
     global maze
     maze = []
@@ -56,93 +67,143 @@ def generateMaze():
                     c.west = m
                     m.east = c
 
+
 tried = []
+
+
 def testMaze(index, path):
     tried[index] = True
-    p = path.copy()
-    p.append(index)
+   
     if maze[index].final:
-        print(p)
+        print(path)
         return True
-    values = [maze[index].east, maze[index].west, maze[index].north, maze[index].south]
-    for val in values:
+    values = [
+        maze[index].north,
+        maze[index].east,
+        maze[index].south,
+        maze[index].west
+    ]
+    for i, val in enumerate(values):
+        p = path.copy()
+        p.append(str(index) + " " + str(i))
         if val != None:
             if not tried[val.index]:
                 if testMaze(val.index, p):
                     return True
     return False
+
+
 def start():
     global tried
     generateMaze()
     tried = [False for i in range(len(maze))]
     return testMaze(0, [])
+
+
 while not start():
     pass
-    
 
-@app.route('/ivr/welcome', methods=['POST'])
+@app.route("/", methods=['GET'])
+def default():
+  return Response("Hello")
+
+@app.route('/maze/go', methods=['POST'])
 def start():
     return menu(0)
 
 
-@app.route('/maze/go/<room>', methods=['POST'])
-def menu(room):
+@app.route('/maze/go/<r>', methods=['POST'])
+def menu(r):
+    room = int(r)
     response = VoiceResponse()
     selectedOption = getDirection(request)
+   
     currentRoom = maze[room]
-    rooms = {1: currentRoom.north, 2: currentRoom.east, 3: currentRoom.south, 4: currentRoom.west, -1: currentRoom}
+    rooms = {
+        2: currentRoom.north,
+        6: currentRoom.east,
+        8: currentRoom.south,
+        4: currentRoom.west,
+        -1: currentRoom
+    }
     currentRoom = rooms[selectedOption]
+    if currentRoom == None:
+      currentRoom = maze[room]
+    print("In room " + str(room) + " going to " + str(selectedOption) + " ended up in room " + str(currentRoom.index))
+   
     if currentRoom.index == room and room != 0:
         _hit_head(response)
     return _give_instructions(response, currentRoom)
 
+
 # private methods
 def getDirection(request):
-    resultMap = {"north": 8, "east": 6, "south": 2, "west": 4}
+    resultMap = {"north": 2, "east": 6, "south": 8, "west": 4}
+    print(request.form.keys())
+    if 'SpeechResult' in request.form.keys():
+        key = request.form['SpeechResult'].lower()
+        print(key)
+        for realKey in resultMap.keys():
+          if realKey in key:
+            return resultMap[realKey]
+            
+        return -1
     if 'Digits' in request.form.keys():
-        key = request.form['Digits']
+        key = int(request.form['Digits'])
         if key % 2 != 0 or key > 8 or key == 0:
             key = -1
         return key
-    if 'SpeechResult' in request.form.keys():
-        key = request.form['SpeechResult']
-        if key not in resultMap.keys():
-            return -1
-        return resultMap[key]
+    
     return -1
+
 
 def twiml(resp):
     resp = Response(str(resp))
     resp.headers['Content-Type'] = 'text/xml'
     return resp
 
+
 def _hit_head(response):
     response.say("You have run straight into a wall, try again.")
 
+
 def _initial_instructions(response):
-    response.say("You are at the entrance of a maze. You can navigate with your keypad or with your voice. Choose wisely.")
+    response.say(
+        "You are at the entrance of a maze. You can navigate with your keypad or with your voice. Choose wisely."
+    )
+
 
 def _final_instructions(response):
     response.say("You have made it to the end of the maze, congratulations!")
     response.hangup()
 
+
 def _add_directions(response, currentRoom):
-    with response.gather(
-        num_digits=1, action='/maze/go/' + str(currentRoom.index), method="POST", input="dtmf speech"
-    ) as g:
+    with response.gather(num_digits=1,
+                         action='/maze/go/' + str(currentRoom.index),
+                         method="POST",
+                         input="dtmf speech") as g:
         g.say(message=_get_room_string(currentRoom))
+
 
 def _get_room_string(currentRoom):
     description = "You are in a room. "
+    debug = "Room: " + str(currentRoom.index)
     if currentRoom.north != None:
         description += "There is a room to the north. "
+        debug += " North: " + str(currentRoom.north.index)
     if currentRoom.east != None:
         description += "There is a room to the east. "
+        debug += " east: " + str(currentRoom.east.index)
     if currentRoom.south != None:
         description += "There is a room to the south. "
+        debug += " south: " + str(currentRoom.south.index)
     if currentRoom.west != None:
         description += "There is a room to the west. "
+        debug += " west: " + str(currentRoom.west.index)
+    print(debug)
     return description
+
 
 def _give_instructions(response, currentRoom):
     if currentRoom.index == 0:
@@ -151,5 +212,8 @@ def _give_instructions(response, currentRoom):
         _final_instructions(response)
     else:
         _add_directions(response, currentRoom)
-    
+
     return twiml(response)
+
+
+app.run(host="0.0.0.0", port=80)
